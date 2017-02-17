95087dc530f362c5915505db6b6e0471f75a1fb5
vharga0074 <vharga0074@WS-AC-CTD-37006>
2017-02-16 19:41:20 -0500

    Add project files.

---
+++ ahhhhh/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ahhhhh
+{
+    [Activity(Label = "Portfolio", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var opt1 = FindViewById<Button>(Resource.Id.opt1);
+            var opt2 = FindViewById<Button>(Resource.Id.opt2);
+            var opt3 = FindViewById<Button>(Resource.Id.opt3);
+            
+                opt1.Click += delegate
+                {
+
+                    var b = FindViewById<TextView>(Resource.Id.entry1).Text;
+                    var f = FindViewById<TextView>(Resource.Id.entry2).Text;
+                    var t = FindViewById<TextView>(Resource.Id.entry3).Text;
+                    double bV = System.Convert.ToDouble(b);
+                    double fV = System.Convert.ToDouble(f);
+                    double tV = System.Convert.ToDouble(t);
+                    var h = (Java.Lang.Math.Log(2) / 2.302585092994046) * tV / (Java.Lang.Math.Log(bV / fV) / 2.302585092994046);
+                    var calculate = FindViewById<Button>(Resource.Id.btnCalc);
+                    calculate.Click += delegate
+                    {
+                        var display = FindViewById<TextView>(Resource.Id.output);
+                        display.Text = "Half-Life: " + h;
+                    };
+                };
+            }
+        }
+    
+}
+
---
+++ ahhhhh/Solution/README.txt
@@ -0,0 +1,6 @@
+﻿YEAHHHHH, GIVING UP!! !
+
+-Create Layout for portrait and landscape | CHECK...sort of (needs to be checked so it stays instead of overwriting each other every time on is editted)
+-Finish coding (math is already sorted out!)
+-figure out the visiblity thing (for the option buttons) (MIGHT LEAVE THEM THERE??)
+-maybe still elevator music, because it's funny.
