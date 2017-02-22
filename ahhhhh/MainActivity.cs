7b8ce48c894075728c2466d34c897372c6b9c599
vharga0074 <vharga0074@WS-AC-CTD-37006>
2017-02-21 21:24:21 -0500

    Separated things into methods (will move to a class file later).

--- ahhhhh/MainActivity.cs
+++ ahhhhh/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using Java.Lang;
+using System;
 
 namespace ahhhhh
 {
@@ -11,61 +12,73 @@ namespace ahhhhh
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            
-
+            SetContentView(Resource.Layout.Main);
             FindViewById<Button>(Resource.Id.opt1).Click += delegate
             {
-                
-                var b = FindViewById<TextView>(Resource.Id.entry1).Text;
-                var f = FindViewById<TextView>(Resource.Id.entry2).Text;
-                var t = FindViewById<TextView>(Resource.Id.entry3).Text;
-                double bV = System.Convert.ToDouble(b);
-                double fV = System.Convert.ToDouble(f);
-                double tV = System.Convert.ToDouble(t);
-                var h = (Math.Log(2) / 2.302585092994046) * tV / (Math.Log(bV / fV) / 2.302585092994046);
-                var calculate = FindViewById<Button>(Resource.Id.btnCalc);
-                calculate.Click += delegate
-                {
-                    var display = FindViewById<TextView>(Resource.Id.textView2);
-                    display.Text = "Half-Life: " + h;
-                };
+                HalfLife();
             };
             FindViewById<Button>(Resource.Id.opt2).Click += delegate
             {
-                FindViewById<TextView>(Resource.Id.entry2).Text ="Half-Life: ";
-                var b = FindViewById<TextView>(Resource.Id.entry1).Text;
-                var h = FindViewById<TextView>(Resource.Id.entry2).Text;
-                var t = FindViewById<TextView>(Resource.Id.entry3).Text;
-                double bV = System.Convert.ToDouble(b);
-                double hV = System.Convert.ToDouble(h);
-                double tV = System.Convert.ToDouble(t);
-                var f = bV / Math.Pow(2, tV / hV);
-                var calculate = FindViewById<Button>(Resource.Id.btnCalc);
-                calculate.Click += delegate
-                {
-                    var display = FindViewById<TextView>(Resource.Id.textView2);
-                    display.Text = "Final Amount: " + f;
-                };
+                FinalAmount();
             };
             FindViewById<Button>(Resource.Id.opt3).Click += delegate
             {
-                FindViewById<TextView>(Resource.Id.entry3).Text = "Half-Life: ";
-                var b = FindViewById<TextView>(Resource.Id.entry1).Text;
-                var f = FindViewById<TextView>(Resource.Id.entry2).Text;
-                var h = FindViewById<TextView>(Resource.Id.entry3).Text;
-                double fV = System.Convert.ToDouble(f);
-                double hV = System.Convert.ToDouble(h);
-                double bV = System.Convert.ToDouble(b);
-                var e = (Math.Log(bV / fV) / 2.302585092994046) / (Math.Log(2) / 2.302585092994046);
-                var calculate = FindViewById<Button>(Resource.Id.btnCalc);
-                calculate.Click += delegate
-                {
-                    var display = FindViewById<TextView>(Resource.Id.textView2);
-                    display.Text = "Elapsed Time: " + e;
-                };
+                ElapsedTime();
             };
+        }
 
-            }
+        private void ElapsedTime()
+        {
+
+            FindViewById<TextView>(Resource.Id.entry3).Text = "Half-Life: ";
+            var b = FindViewById<TextView>(Resource.Id.entry1).Text;
+            var f = FindViewById<TextView>(Resource.Id.entry2).Text;
+            var h = FindViewById<TextView>(Resource.Id.entry3).Text;
+            double fV = Convert.ToDouble(f);
+            double hV = Convert.ToDouble(h);
+            double bV = Convert.ToDouble(b);
+            var e = (Java.Lang.Math.Log(bV / fV) / 2.302585092994046) / (Java.Lang.Math.Log(2) / 2.302585092994046);
+            var calculate = FindViewById<Button>(Resource.Id.btnCalc);
+            calculate.Click += delegate
+            {
+                var display = FindViewById<TextView>(Resource.Id.textView2);
+                display.Text = "Elapsed Time: " + e;
+            };
+        }
+
+        private void FinalAmount()
+        {
+            FindViewById<TextView>(Resource.Id.entry2).Text = "Half-Life: ";
+            var b = FindViewById<TextView>(Resource.Id.entry1).Text;
+            var h = FindViewById<TextView>(Resource.Id.entry2).Text;
+            var t = FindViewById<TextView>(Resource.Id.entry3).Text;
+            double bV = Convert.ToDouble(b);
+            double hV = Convert.ToDouble(h);
+            double tV = Convert.ToDouble(t);
+            var f = bV / Java.Lang.Math.Pow(2, tV / hV);
+            var calculate = FindViewById<Button>(Resource.Id.btnCalc);
+            calculate.Click += delegate
+            {
+                var display = FindViewById<TextView>(Resource.Id.textView2);
+                display.Text = "Final Amount: " + f;
+            };
+        }
+
+        public void HalfLife()
+        {
+            var b = FindViewById<TextView>(Resource.Id.entry1).Text;
+            var f = FindViewById<TextView>(Resource.Id.entry2).Text;
+            var t = FindViewById<TextView>(Resource.Id.entry3).Text;
+            double bV = Convert.ToDouble(b);
+            double fV = Convert.ToDouble(f);
+            double tV = Convert.ToDouble(t);
+            var h = (Java.Lang.Math.Log(2) / 2.302585092994046) * tV / (Java.Lang.Math.Log(bV / fV) / 2.302585092994046);
+            var calculate = FindViewById<Button>(Resource.Id.btnCalc);
+            calculate.Click += delegate
+            {
+                var display = FindViewById<TextView>(Resource.Id.textView2);
+                display.Text = "Half-Life: " + h;
+            };
         }
     }
-
+}
